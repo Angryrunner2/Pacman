425a2ca349f198e079d9fb29b0846d2367a5e25c
Gabriel <67304635+Angryrunner2@users.noreply.github.com>
2020-09-21 23:47:40 +1000

    Add files via upload

---
+++ AnimationChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChange : MonoBehaviour
+{
+    public Animator animatorController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+                animatorController.SetTrigger("goDown");
+        }
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                animatorController.SetTrigger("goLeft");
+        }
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                animatorController.SetTrigger("goRight");
+        }
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                animatorController.SetTrigger("goUp");
+        }
+    }
+}
---
+++ music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    AudioSource audioSource;
+   
+    public List<AudioClip> clips = new List<AudioClip>();
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        clips.Add((AudioClip)Resources.Load(" Sound/openning "));
+        clips.Add((AudioClip)Resources.Load(" Sound/normal "));
+        clips.Add((AudioClip)Resources.Load(" Sound/scared "));
+        clips.Add((AudioClip)Resources.Load(" Sound/dead "));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = Resources.Load<AudioClip>("Sound/normal");
+            audioSource.Play();
+        }
+    }
+}
